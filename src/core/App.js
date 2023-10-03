3145616b976236b11f8464188f0c04ed6c564e1f
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-10-03 20:21:36 +0200

    added components to app

--- src/core/App.js
+++ src/core/App.js
@@ -1,3 +1,14 @@
-const App = () => <></>;
+import { Header } from "../common/header";
+import { Main } from "../common/main";
+import ShipCollection from "../features/ShipCollection";
+
+const App = () => (
+  <>
+    <Header>Battleships</Header>
+    <Main>
+      <ShipCollection />
+    </Main>
+  </>
+);
 
 export default App;
