dc46193238d8cedb20664bbb3625046726824965
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-10-03 20:20:44 +0200

    created header

---
+++ src/common/header.js
@@ -0,0 +1,10 @@
+import { styled } from "styled-components";
+
+export const Header = styled.h1`
+  font-family: "Montserrat";
+  color: white;
+  font-size: 64px;
+  text-align: center;
+  font-weight: 300;
+  user-select: none;
+`;
