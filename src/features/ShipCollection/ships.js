81726b3d326ebb3a8c04be4e4ceb000f9f605073
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-10-03 20:21:08 +0200

    created ships

---
+++ src/features/ShipCollection/ships.js
@@ -0,0 +1,29 @@
+import { styled, css } from "styled-components";
+
+export const Destroyer = styled.div`
+  height: 10px;
+  width: 40px;
+  background-color: white;
+  border-radius: 4px;
+  padding: 2px;
+  cursor: pointer;
+  ${({ flipped }) =>
+    flipped &&
+    css`
+      transform: rotate(90deg);
+    `}
+`;
+
+export const Thunder = styled(Destroyer)`
+  width: 50px;
+`;
+
+export const Wrecker = styled(Destroyer)`
+  width: 30px;
+`;
+
+export const Liberty = styled(Wrecker)``;
+
+export const Sapphire = styled(Destroyer)`
+  width: 20px;
+`;
