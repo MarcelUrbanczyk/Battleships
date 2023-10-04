91b4bd9052d87353826284015e1e7e3f47e1b059
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-10-04 21:22:33 +0200

    added owner to every board

--- src/features/Boards/index.js
+++ src/features/Boards/index.js
@@ -2,7 +2,7 @@ import { Wrapper } from "./styled";
 import Board from "./Board";
 export default () => (
   <Wrapper>
-    <Board />
-    <Board />
+    <Board owner="Player 1" />
+    <Board owner="Player 2" />
   </Wrapper>
 );
