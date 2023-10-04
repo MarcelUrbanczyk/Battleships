f47f7aa82736f211896a0ff48f89c5912895394e
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-10-04 21:01:33 +0200

    added boards to app

--- src/core/App.js
+++ src/core/App.js
@@ -1,11 +1,13 @@
 import { Header } from "../common/header";
 import { Main } from "../common/main";
+import Boards from "../features/Boards";
 import ShipCollection from "../features/ShipCollection";
 
 const App = () => (
   <>
     <Header>Battleships</Header>
     <Main>
+      <Boards />
       <ShipCollection />
     </Main>
   </>
