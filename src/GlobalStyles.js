80ec2abc50f0a924d2205830cdbcae491489a124
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-10-04 21:22:01 +0200

    changed font in global styles

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -5,9 +5,7 @@ body {
   background-color: #4F6272;
   margin: 0;
   box-sizing: border-box;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
+  font-family: 'Montserrat';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
