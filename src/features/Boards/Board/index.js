9b59c767205ee2f8771c3b3be839b09116ac2911
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-10-04 21:23:11 +0200

    added owner to board

--- src/features/Boards/Board/index.js
+++ src/features/Boards/Board/index.js
@@ -1,9 +1,12 @@
-import { Container, Block } from "./styled";
+import { Container, Block, Owner, Wrapper } from "./styled";
 
-export default () => (
-  <Container>
-    {Array.from({ length: 100 }, (_, index) => (
-      <Block key={index}></Block>
-    ))}
-  </Container>
+export default ({ owner }) => (
+  <Wrapper>
+    <Owner>{owner}</Owner>
+    <Container>
+      {Array.from({ length: 100 }, (_, index) => (
+        <Block key={index}></Block>
+      ))}
+    </Container>
+  </Wrapper>
 );
