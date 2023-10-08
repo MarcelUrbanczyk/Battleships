e021549b24b7dd59239d6fe68367dca9ca15a372
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-10-08 19:52:28 +0200

    created array of ship objects

---
+++ src/features/ships.js
@@ -0,0 +1,27 @@
+export const ships = [
+  {
+    name: "Destroyer",
+    size: 4,
+    color: "red",
+  },
+  {
+    name: "Thunder",
+    size: 5,
+    color: "blue",
+  },
+  {
+    name: "Wrecker",
+    size: 3,
+    color: "green",
+  },
+  {
+    name: "Liberty",
+    size: 3,
+    color: "orange",
+  },
+  {
+    name: "Sapphire",
+    size: 2,
+    color: "purple",
+  },
+];
