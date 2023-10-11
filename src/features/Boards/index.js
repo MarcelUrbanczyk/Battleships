fbc8c6646649d69c1a44a6a42592892faf301117
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-10-11 18:58:17 +0200

    boards are now receiving states

--- src/features/Boards/index.js
+++ src/features/Boards/index.js
@@ -1,8 +1,21 @@
 import { Wrapper } from "./styled";
 import Board from "./Board";
-export default () => (
+export default ({
+  firstBoard,
+  secondBoard,
+  setFirstBoard,
+  setSecondBoard,
+  draggedShip,
+  flip,
+}) => (
   <Wrapper>
-    <Board owner="Player 1" />
-    <Board owner="Player 2" />
+    <Board
+      owner="Player 1"
+      draggedShip={draggedShip}
+      board={firstBoard}
+      setBoard={setFirstBoard}
+      flip={flip}
+    />
+    <Board owner="Computer" board={secondBoard} setBoard={setSecondBoard} />
   </Wrapper>
 );
