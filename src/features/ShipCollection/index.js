17171e6d1ae28a61e2aedbc831fbc98e2a31a2fc
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-10-11 21:32:02 +0200

    ship collection now renders in condition that at least one ship isnt dropped

--- src/features/ShipCollection/index.js
+++ src/features/ShipCollection/index.js
@@ -1,34 +1,40 @@
 import { Container, FlipButton, ShipWrapper, Header, Ship } from "./styled";
-import { ships } from "../ships";
 
-export default ({ setDraggedShip, flip, setFlip }) => {
-  return (
-    <>
-      <Header>Ships</Header>
-      <Container>
-        <ShipWrapper>
-          {ships.map((ship) => (
-            <Ship
-              key={ship.name}
-              id={ship.name}
-              draggable
-              flipped={flip}
-              style={{
-                backgroundColor: ship.color,
-                width: `${ship.size * 10}px`,
-              }}
-              onDragStart={(event) => {
-                setDraggedShip(event.target);
-              }}
-            />
-          ))}
-        </ShipWrapper>
-        <FlipButton
-          onClick={() => {
-            setFlip(!flip);
-          }}
-        />
-      </Container>
-    </>
-  );
+export default ({ setDraggedShip, flip, setFlip, ships }) => {
+  const isEveryShipDropped = ships.every((ship) => ship.isDropped === true);
+  if (!isEveryShipDropped)
+    return (
+      <>
+        <Header>Ships</Header>
+        <Container>
+          <ShipWrapper>
+            {ships.map((ship) => {
+              if (!ship.isDropped) {
+                return (
+                  <Ship
+                    key={ship.name}
+                    id={ship.name}
+                    draggable
+                    flipped={flip}
+                    style={{
+                      backgroundColor: ship.color,
+                      width: `${ship.size * 10}px`,
+                    }}
+                    onDragStart={(event) => {
+                      setDraggedShip(event.target);
+                    }}
+                  />
+                );
+              }
+              return null;
+            })}
+          </ShipWrapper>
+          <FlipButton
+            onClick={() => {
+              setFlip(!flip);
+            }}
+          />
+        </Container>
+      </>
+    );
 };
