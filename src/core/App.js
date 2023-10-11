334375543e20053fdc374b41d6df1c662e13a524
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-10-11 18:58:31 +0200

    states moved to app js

--- src/core/App.js
+++ src/core/App.js
@@ -2,15 +2,43 @@ import { Header } from "../common/header";
 import { Main } from "../common/main";
 import Boards from "../features/Boards";
 import ShipCollection from "../features/ShipCollection";
+import { useState } from "react";
 
-const App = () => (
-  <>
-    <Header>Battleships</Header>
-    <Main>
-      <Boards />
-      <ShipCollection />
-    </Main>
-  </>
-);
+const App = () => {
+  const size = 10;
+
+  const [flip, setFlip] = useState(false);
+
+  const [draggedShip, setDraggedShip] = useState(null);
+
+  const [firstBoard, setFirstBoard] = useState(
+    Array.from({ length: size * size }).fill(null)
+  );
+
+  const [secondBoard, setSecondBoard] = useState(
+    Array.from({ length: size * size }).fill(null)
+  );
+
+  return (
+    <>
+      <Header>Battleships</Header>
+      <Main>
+        <Boards
+          firstBoard={firstBoard}
+          secondBoard={secondBoard}
+          setFirstBoard={setFirstBoard}
+          setSecondBoard={setSecondBoard}
+          draggedShip={draggedShip}
+          flip={flip}
+        />
+        <ShipCollection
+          setDraggedShip={setDraggedShip}
+          flip={flip}
+          setFlip={setFlip}
+        />
+      </Main>
+    </>
+  );
+};
 
 export default App;
