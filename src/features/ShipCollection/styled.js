837957fa320d4dad0992d953a0613a98d5afd6a8
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-10-11 21:31:22 +0200

    changed background color and added box shadow to ship collection container

--- src/features/ShipCollection/styled.js
+++ src/features/ShipCollection/styled.js
@@ -17,8 +17,12 @@ export const Ship = styled.div`
 
 export const Container = styled.div`
   align-items: center;
+  background-color: #3b4752;
   border-radius: 8px;
-  border: 1px solid white;
+  border: 2px solid white;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+    rgba(255, 255, 255, 0.3) 0px 30px 60px -30px,
+    rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
   display: flex;
   justify-content: space-between;
   padding: 30px;
@@ -41,7 +45,7 @@ export const FlipButton = styled(FlipIcon)`
 
 export const Header = styled.h2`
   color: white;
-  font-weight: 300;
+  font-weight: 400;
   margin: 5px;
   user-select: none;
 `;
