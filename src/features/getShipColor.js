9a39a96fd5413cef4d3952caf7d51acb46db8312
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-10-12 22:08:13 +0200

    ship color is different if its sunk

--- src/features/getShipColor.js
+++ src/features/getShipColor.js
@@ -1,9 +1,10 @@
-import { initialShips } from "./ships";
-
-export const getShipColor = (shipName, owner) => {
+export const getShipColor = (shipName, owner, ships) => {
   if (owner !== "Computer") {
-    const ship = initialShips.find((ship) => ship.name === shipName);
-    if (ship) {
+    const ship = ships.find((ship) => ship.name === shipName);
+    if (ship && ship.isSunk) {
+      const color = "grey";
+      return color;
+    } else if (ship && !ship.isSunk) {
       const color = ship.color;
       return color;
     }
