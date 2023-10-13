9d37ef4b1ae20a43f0c27154c9f5c5b3d960a4e5
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-10-14 01:48:48 +0200

    moved state to redux

--- src/core/App.js
+++ src/core/App.js
@@ -2,60 +2,13 @@ import { Header } from "../common/header";
 import { Main } from "../common/main";
 import Boards from "../features/Boards";
 import ShipCollection from "../features/ShipCollection";
-import { useState } from "react";
-import { initialShips } from "../features/initialShips";
-import { initialBoard } from "../features/initialBoard";
-
 const App = () => {
-  const [gameState, setGameState] = useState({
-    ships1: [...initialShips],
-    ships2: [...initialShips],
-    isGameStarted: false,
-    isGameOver: false,
-    isPlayer1Turn: false,
-    flip: false,
-    draggedShip: null,
-    board1: [...initialBoard],
-    board2: [...initialBoard],
-  });
-
-  console.log(gameState);
-
-  const {
-    ships1,
-    ships2,
-    isGameStarted,
-    isGameOver,
-    isPlayer1Turn,
-    flip,
-    draggedShip,
-    board1,
-    board2,
-  } = gameState;
-
   return (
     <>
       <Header>Battleships</Header>
       <Main>
-        <Boards
-          board1={board1}
-          board2={board2}
-          draggedShip={draggedShip}
-          flip={flip}
-          ships1={ships1}
-          ships2={ships2}
-          isPlayer1Turn={isPlayer1Turn}
-          isGameStarted={isGameStarted}
-          isGameOver={isGameOver}
-          gameState={gameState}
-          setGameState={setGameState}
-        />
-        <ShipCollection
-          flip={flip}
-          ships={ships1}
-          gameState={gameState}
-          setGameState={setGameState}
-        />
+        <Boards />
+        <ShipCollection />
       </Main>
     </>
   );
