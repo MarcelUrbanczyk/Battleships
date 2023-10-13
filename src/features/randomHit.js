5b3a5ae51d21cd601f3cd6f426a1ea6758a0c07d
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-10-14 01:51:41 +0200

    created random hit function

---
+++ src/features/randomHit.js
@@ -0,0 +1,16 @@
+export const randomHit = (owner) => {
+  let blocksToAttack = document.getElementsByClassName(`${owner}`);
+
+  if (blocksToAttack.length > 0) {
+    const randomIndex = Math.floor(Math.random() * blocksToAttack.length);
+    let blockToAttack = blocksToAttack[randomIndex];
+
+    if (blockToAttack.classList.contains("hit")) {
+      blockToAttack =
+        blocksToAttack[Math.floor(Math.random() * blocksToAttack.length)];
+    } else if (!blockToAttack.classList.contains("hit")) {
+      console.log(1);
+      blockToAttack.classList.add("hit");
+    }
+  }
+};
