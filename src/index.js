59b52101706f007c6733c3390067a202b54b2ed3
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-10-14 01:48:31 +0200

    added provider and store to index js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./core/App";
 import reportWebVitals from "./reportWebVitals";
 import { GlobalStyle } from "./GlobalStyles";
+import { Provider } from "react-redux";
+import store from "./core/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <Provider store={store}>
+      <GlobalStyle />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
