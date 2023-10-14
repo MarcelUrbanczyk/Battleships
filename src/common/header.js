88e7e32e577ff66e6b407405371bb90221c13314
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-10-14 22:17:02 +0200

    changed header font size

--- src/common/header.js
+++ src/common/header.js
@@ -3,7 +3,7 @@ import { styled } from "styled-components";
 export const Header = styled.h1`
   color: white;
   font-family: "Montserrat";
-  font-size: 64px;
+  font-size: 72px;
   font-weight: 300;
   text-align: center;
   user-select: none;
